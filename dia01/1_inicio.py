7e662265d15fec49cb873692a17a01b4c9c285d5
franklin peña <franklin.pena@tecsup.edu.pe>
2024-01-11 21:05:27 -0500

    Iniciando en python

---
+++ dia01/1_inicio.py
@@ -0,0 +1,79 @@
+nombre = 'Eduardo'
+nombre = 12
+nombre =  False
+nombre = '2023-12-12'
+
+# Creo la variable para indicar el estado civil de la persona
+
+soltero = False
+
+# El comentario es muy importante para mantener nuestro codigo documentado
+
+print(nombre)
+
+numero1 = 10
+numero2 = 40
+
+resultado = numero1 + numero2
+print(resultado)
+
+numero1 = '80' 
+numero2 = '40'
+
+print(type(numero1))
+
+# Si queremos realizar una sumatoria entre un texto y un numero no es posible
+# Si la sumatoria se realiza entre dos string (texto) esta sera una concatenacion
+
+resultado = numero1 + numero2
+print(resultado)
+
+# Para convertir de un dato a otro utilizo el tipo de dato que quiro convertir
+# Tengo que estar seguro que el valor fuente pueda ser convertido
+
+convertir_entero = int(numero1)
+
+print(type(convertir_entero))
+
+
+# TIPOS DE VARIABLES
+
+# INT (Enteros)
+
+numero = 10
+
+# FLOAT (Flotantes)
+
+calificacion = 10.4
+
+# BOOLEAN (Boolean)
+
+vacunado = False
+
+# STRING (texto)
+
+nombre = 'eduardo"carnicero"'
+nombre = "eduard'o"
+texto = '''
+Era una mañana de enero.
+Los pajaros cantaban ...
+'''
+print(texto)
+
+texto2 = """
+Y en eso, una señora
+Muy amablemente me sonrio y me dijo "..."
+"""
+
+# Si queremos que el texto contenga saltos en linea usamos la triple comilla o triple doble comilla
+
+curso, grupo, mes, dia, hora, minuto = 'Backend', 'G16', 'Enero', 11, 20, 45
+
+# FORMA DE DECLARAR VARIABLES, FUNCIONES, CLASES, METODOS
+
+snake_case = 'snake_case'
+nombre_completo = 'Franklin Peña Quispe'
+
+camelCase = 'camelCase'
+
+PascalCase = 'PascalCase'
---
+++ dia01/2_colecciones_de_datos.py
@@ -0,0 +1,20 @@
+# Puedo Agrupar varios valores en una variable
+
+# Listas
+# Que se puede modificar, es ordenada (maneja indices)
+
+alumnos = ['victor', 'Hiroito', 'Marco', 'Angel', 'Bryan', 'Samael', 'Claudia']
+
+# Las listas empiezan con la posicion 0
+
+print(alumnos[0])
+print(alumnos[4])
+
+# Para saber el contenido (logitud) de datos
+# Cuenta y no utiliza las posiciones
+
+print(len(alumnos))
+
+# Si queremos contar la lista de derecha a izquierda utilizaremos numeros negativos
+
+print(alumnos[-1])
